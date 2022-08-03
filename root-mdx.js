79c6d7a16415732b1305c338264fc7a67c2a0686
Fc0S4b <fcoj.sabugo@gmail.com>
2022-08-03 02:23:49 -0400

    inline code

--- root-mdx.js
+++ root-mdx.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings } from './src/components/Complete'
+import { Headings, Code } from './src/components/Complete'
+
+const Testing = ({ children }) => {
+  return <code className="code">{children}</code> //code inside in a <p>, error with divs
+}
 
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
+  // inlineCode: Testing,
+  inlineCode: Code,
 }
 
 export const wrapMDX = ({ element }) => {
